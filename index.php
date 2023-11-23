9f1aef5a75047fd4dddfd671cfc19afd2a2898cb
Oleksandr Krynytskyi <neomancer2@gmail.com>
2023-11-23 10:14:05 +0200

    css version changed

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['user_id'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/style/style.min.css">
+  <link rel="stylesheet" href="/style/style.min.css?1">
   <script src="/scripts/jquery-3.7.1.min.js"></script>
   <script src="/scripts/sweetalert2@11.js"></script>
   <link rel="icon" href="./style/images/paw-icon.png" type="image/x-icon">
